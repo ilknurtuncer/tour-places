3844b2ee28e14523d16766b536ad088d1aeccf3f
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-02-21 18:38:39 +0300

    main created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import "./App.scss"
 import Header from './components/header/Header';
+import Main from './components/main/Main';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
    <Navbar/>
    <Header/>
+   <Main/>
     </div>
   );
 }
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,11 @@
+import "./Main.scss"
+
+const Main = () => {
+  return (
+    <div>main
+      
+    </div>
+  )
+}
+
+export default Main
